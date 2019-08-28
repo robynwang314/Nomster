c9fa70a9d36baa6eeaedc5d5a3850a8cbcf3fa93
Robyn Wang <tyrobynwang@gmail.com>
2019-08-28 23:23:35 +0000

    nomster 23

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,13 @@ def index
       @place = Place.find(params[:id])
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+
 
   private
 
