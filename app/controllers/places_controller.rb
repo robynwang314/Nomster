cd25612182733ca2fc9741f41b01acfda57809a0
Robyn Wang <tyrobynwang@gmail.com>
2019-08-29 00:36:40 +0000

    nomster 27

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ def index
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -33,7 +37,11 @@ def index
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -41,7 +49,7 @@ def index
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end 
-    
+
     @place.destroy
     redirect_to root_path
   end
