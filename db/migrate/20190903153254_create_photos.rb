c9f0ee2ee4821ea598d859488bed7a6024323afb
Robyn Wang <tyrobynwang@gmail.com>
2019-09-12 11:42:21 +0000

    nomster 39 .1

--- db/migrate/20190903153254_create_photos.rb
+++ db/migrate/20190903153254_create_photos.rb
@@ -5,5 +5,8 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
       t.integer :place_id
       t.timestamps
     end
+
+    add_index :photos, [:user_id, :place_id]
+    add_index :photos, :place_id
   end
 end
