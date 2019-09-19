51aa0180dee6d0473dd0c22d508b4e636d4993fb
Robyn Wang <tyrobynwang@gmail.com>
2019-09-19 19:08:36 +0000

    nomster 39 captions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,7 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params.merge(user: current_user))
+    @place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
 
@@ -11,5 +11,6 @@ class PhotosController < ApplicationController
 
   def photo_params
       params.require(:photo).permit(:caption)
-    end
+  end
 end
+
