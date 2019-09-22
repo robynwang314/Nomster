1d8ba067289b3f6bb58df98a28c0387e61db493a
Robyn Wang <tyrobynwang@gmail.com>
2019-09-22 21:26:29 +0000

    nomster 39 resetted db

--- db/migrate/20190903153254_create_photos.rb
+++ db/migrate/20190903153254_create_photos.rb
@@ -3,6 +3,7 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
     create_table :photos do |t|
       t.text :caption
       t.integer :place_id
+      t.integer :user_id
       t.timestamps
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,12 @@ ActiveRecord::Schema.define(version: 2019_09_19_192532) do
   create_table "photos", force: :cascade do |t|
     t.text "caption"
     t.integer "place_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
 
   create_table "places", force: :cascade do |t|
