25236096a5d87370fce912b749c2d2b57bae6cbf
Robyn Wang <tyrobynwang@gmail.com>
2019-09-27 23:31:04 +0000

    nomster 42

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,11 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "my@mail.com",
-          subject: "A comment has been added to your")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+          subject: "A comment has been added to #{@place.name}")
   end
 
 end
