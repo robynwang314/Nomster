f538a60a160b28d61d7104e30e5757d080a3baec
Robyn Wang <tyrobynwang@gmail.com>
2019-09-28 02:36:18 +0000

    nomster 44 .2

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Nomster
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
